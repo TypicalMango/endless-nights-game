c34d1f0d61989188446de59368a21cc9bdc0d146
TypicalMango <adityaguleria010@gmail.com>
2024-11-02 20:09:34 +0530

    adding fov to enemies (status: non-functional)

--- Assets/Scripts/Enemy/PlayerAwarenessController.cs
+++ Assets/Scripts/Enemy/PlayerAwarenessController.cs
@@ -6,24 +6,48 @@ public class PlayerAwarenessController : MonoBehaviour
 {
     public bool AwareOfPlayer { get; private set; }
     public Vector2 DirectionToPlayer { get; private set; }
-    [SerializeField]
-    private float _playerAwarenessDistance;
+    // [SerializeField]
+    // private float _playerAwarenessDistance;
     private Transform _player;
     private void Awake()
     {
         _player = FindObjectOfType<PlayerController>().transform;
     }
+
+
     // Update is called once per frame
     void Update()
     {
-        Vector2 enemyToPlayerVector = _player.position - transform.position;
+        IsTargetInsideFOV(_player);
+    //     Vector2 enemyToPlayerVector = _player.position - transform.position;
+    //     DirectionToPlayer = enemyToPlayerVector.normalized;
+
+    //     if (enemyToPlayerVector.magnitude <= _playerAwarenessDistance) {
+    //         AwareOfPlayer = true;
+    //     }
+    //     else {
+    //         AwareOfPlayer = false;
+    //     }
+    }
+
+    [SerializeField]
+    private float fovAngle;
+    [SerializeField]
+    private float fovRange;
+
+    private Vector2 lookDirection = Vector2.up;
+
+    private void IsTargetInsideFOV(Transform target)
+    {
+        Vector2 enemyToPlayerVector = target.position - transform.position;
         DirectionToPlayer = enemyToPlayerVector.normalized;
 
-        if (enemyToPlayerVector.magnitude <= _playerAwarenessDistance) {
+        float angleToPlayer = Vector2.Angle(lookDirection, DirectionToPlayer);
+
+        if (angleToPlayer < fovAngle / 2 && enemyToPlayerVector.magnitude <= fovRange)
+        {
             AwareOfPlayer = true;
-        }
-        else {
-            AwareOfPlayer = false;
-        }
+        } else { AwareOfPlayer = false; }
     }
+
 }
